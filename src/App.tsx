255866ef1f8bb9b3cf4148e5507228e5521e64e9
asdf0563 <3663587358@qq.com>
2026-03-26 18:12:16 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Header from './components/Header';
+import Nav from './components/Nav';
+import MainContent from './components/MainContent';
+import Sidebar from './components/Sidebar';
+import Footer from './components/Footer';
+import styles from './App.module.css'; // CSS Module 导入
+
+
+const App: React.FC = () => {
+  return (
+    // 使用 Grid 布局构建页面骨架
+    <div className={styles.pageLayout}>
+      <Header />
+      <Nav />
+      <MainContent />
+      <Sidebar />
+      {<Footer /> }
+    </div>
+  );
+};
+
+export default App;
